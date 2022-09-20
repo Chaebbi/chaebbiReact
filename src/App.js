448f31bc2798aaaf7e9671f62805dc5cb21b8c27
idx123 <jinhalee020@gmail.com>
2022-09-21 04:14:52 +0900

    [Fix] 루트 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import Navigation from "./components/Navigation";
-import Test from "./pages/Test";
 import Landing from "./pages/Landing";
 import UserLogin from "./pages/UserLogin";
 import UserRegister from "./pages/UserRegister";
@@ -15,59 +14,36 @@ import AnalyzeDiet from "./pages/AnalyzeDiet";
 import Detail from "./pages/Detail";
 import KakaoUserLogin from "./pages/KakaoUserLogin";
 import OnBoard from "./pages/OnBoard";
+import PrivateRoute from "./components/Routes/PrivateRoute";
+import isLogin from "../src/utils/isLogin";
 
 function App() {
-  const insertedToken = localStorage.getItem('token');
+  // const insertedToken = localStorage.getItem('token');
   return (
     <Container>
-      <BrowserRouter>
+      <Router>
         <Navigation/>
-
         <Subcontainer>
           <Routes>
-            {insertedToken ?
-              <Route path='/' element={<Main/>}></Route>
-              : 
-              <Route path='/' element={<Landing/>}></Route>
-            }
-            <Route path='/analyze-diet' element={<AnalyzeDiet/>}></Route>
-            
-            <Route path='/sign_up' element={<UserRegister/>}></Route>
-            <Route path='/sign_in' element={<UserLogin/>}></Route>
-            <Route path='/kakao_login' element={<KakaoUserLogin/>}></Route>
+            <Route element={<PrivateRoute />}>
+              <Route path='/' element={<Main />} exact/>
+              <Route path='/analyze-diet' element={<AnalyzeDiet/>}/>
+              <Route path='/record-foodsearch'element={<RecordSearch/>}/>
+              <Route path='/record-foodimage'element={<RecordImage/>}/>
+              <Route path='/detail/:r_id' element={<Detail/>}/>
+              <Route path='/search-restaurant' element={<SearchRestaurant/>}></Route>
+              <Route path='/recommend-fridge-ingredients' element={<RecommendFridge/>}></Route>
+              <Route path='/mypage' element={<Mypage/>}></Route><Route path='/' element={<Landing/>}></Route>
+            </Route>
+          
+            <Route path='/landing' element={<Landing/>}/>
+            <Route path='/sign_up' element={<UserRegister/>}/>
+            <Route path='/sign_in' element={<UserLogin/>}/>
+            <Route path='/kakao_login' element={<KakaoUserLogin/>}/>
             <Route path='/on_board' element={<OnBoard/>}></Route>
-
-            <Route path='/record-foodsearch'element={<RecordSearch/>}></Route>
-            <Route path='/record-foodimage'element={<RecordImage/>}></Route>
-            <Route path='/detail/:r_id' element={<Detail/>}></Route>
-
-            <Route path='/search-restaurant' element={<SearchRestaurant/>}></Route>
-            <Route path='/recommend-fridge-ingredients' element={<RecommendFridge/>}></Route>
-
-            <Route path='/mypage' element={<Mypage/>}></Route>
-            <Route path='/test' element={<Test/>}></Route>
           </Routes>
-
-
-          {/* <PublicRoute restricted={true} component={UserRegister} path="/sign_up" exact />
-          <PublicRoute restricted={true} component={UserLogin} path="/sign_in" exact />
-          <PublicRoute restricted={true} component={KakaoUserLogin} path="/kakao_login" exact />
-          <PublicRoute restricted={true} component={OnBoard} path="/on_board" exact />
-
-          <PrivateRoute component={AnalyzeDiet} path="/analyze-diet" exact /> 
-
-            <PrivateRoute component={RecordSearch} path="/record-foodsearch" exact /> 
-            <PrivateRoute component={RecordSearch} path="/record-foodsearch" exact /> 
-            <PrivateRoute component={RecordImage} path="/record-foodimage" exact /> 
-            <PrivateRoute component={Detail} path="/detail" exact /> 
-
-            <PrivateRoute component={SearchRestaurant} path="/search-restaurant" exact /> 
-            <PrivateRoute component={RecommendFridge} path="/recommend-fridge-ingredients" exact /> 
-
-            <PrivateRoute component={Mypage} path="/mypage" exact /> 
-            <Route component={NotFound}/> */}
         </Subcontainer>
-      </BrowserRouter>
+      </Router>
     </Container>
   );
 }
