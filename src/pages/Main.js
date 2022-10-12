c8286709d4e037d883161cd8f379ad3661f6cff8
idx123 <jinhalee020@gmail.com>
2022-10-12 17:47:48 +0900

    [Fix] 일정등록 컴포넌트 제거 및 스타일 변경

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,23 +1,12 @@
-import { useState,useEffect } from "react";
-import styled from "styled-components";
-import Grid from "../elements/Grid";
-import MealRecord from "../components/MealRecord";
 import Planner from "../components/Planner";
-import NutrientsInfo from "../components/NutrientsInfo";
 
-//페이지 설명
+//로그인 후 메인 페이지
 function Main(){
-
     return(
-        <MainContainer>
+        <div style={{minWidth: "650px", margin: "0 auto"}}>
             <Planner/>
-        </MainContainer>
+        </div>
         )
     }
-
-
-const MainContainer = styled.div`
-    min-width: 650px;
-    margin: 0 auto;
-`;
+    
 export default Main;
