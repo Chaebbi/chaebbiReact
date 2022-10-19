227dc8db51b5fd593fe3e6961840cd449529e22b
idx123 <jinhalee020@gmail.com>
2022-10-20 00:29:46 +0900

    [Fix] 프로필, 북마크 페이지 분리 및 탭메뉴 추가

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,13 +1,60 @@
 import Profile from "../components/Profile";
 import Bookmarks from "../components/Bookmarks";
+import styles from "../styles/pages/MyPage.module.css";
+import {useState,useEffect} from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
-//마이페이지==> 상단엔 프로필, 하단엔 즐겨찾기 조회
+//마이페이지
 function Mypage(){
+    const [isProfile,setIsProfile] = useState(true);
+    const [isBookmarks,setIsBookmarks] = useState(false);
+    const navigate = useNavigate();
+    
+    const changeRadio =(e)=>{
+        if(e.target.value == "profile"){
+            setIsProfile(true);
+            setIsBookmarks(false);
+            console.log(e.target.value);
+        }else{
+            setIsProfile(false);
+            setIsBookmarks(true);
+            console.log(e.target.value);
+        }
+    }
+
+    useEffect(()=>{
+        
+    },[])
+
     return(
-        <>
-            <Profile/>
-            <Bookmarks/>
-        </>
+        <div className={styles.container}>
+            <div className={styles.radioContainer}>
+                <input 
+                    id="1" 
+                    name="tab" 
+                    type="radio"
+                    onChange={changeRadio}
+                    value="profile"
+                    defaultChecked
+                    className={isProfile ? `${styles.active}` : null} />
+                <label htmlFor="1">내정보</label>
+                <input 
+                    id="2" 
+                    name="tab" 
+                    type="radio" 
+                    onChange={changeRadio} 
+                    value="bookmarks"
+                    className={isBookmarks ? `${styles.active}` : null} />
+                <label htmlFor="2">북마크</label>
+            </div>
+            <div className={isProfile ? `${styles.activeContent}` : `${styles.inactive}`} >
+                <Profile/>  
+            </div>
+            <div className={isBookmarks ? `${styles.activeContent}` : `${styles.inactive}`} >
+                <Bookmarks/>    
+            </div>
+        </div>
     )
 }
 
