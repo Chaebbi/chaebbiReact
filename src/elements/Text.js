f44e8b14a79bcea564f14f049c90871a66c8875d
idx123 <jinhalee020@gmail.com>
2022-10-21 15:03:40 +0900

    [Fix] Text 스타일 선택지 추가(inline,center)

--- src/elements/Text.js
+++ src/elements/Text.js
@@ -11,31 +11,69 @@ function Text(props){
         position,
         top,left,right,bottom,
         background,
+        inline,
+        center,
         label,
         text
     } = props;
 
+    if(inline){
+        return(
+            <InlineTextContainer
+                inline
+                width={width} 
+                margin={margin}
+                position={position} 
+                top={top}
+                bottom={bottom}
+                left={left}
+                right={right}
+                background={background}>
+                    <InlineH4 labelsize={labelsize}>{label}</InlineH4>
+                    <P fontsize={fontsize} fontweight={fontweight}>{text}</P>
+            </InlineTextContainer>
+            );
+    }
+
+    if(center){
+        return(
+            <CenterTextContainer
+                center
+                width={width} 
+                margin={margin}
+                position={position} 
+                top={top}
+                bottom={bottom}
+                left={left}
+                right={right}
+                background={background}>
+                    <InlineH4 labelsize={labelsize}>{label}</InlineH4>
+                    <P fontsize={fontsize} fontweight={fontweight}>{text}</P>
+            </CenterTextContainer>
+            );
+    }
+
     return(
-        <>
             <TextContainer 
-            width={width} 
-            margin={margin}
-            position={position} 
-            top={top}
-            bottom={bottom}
-            left={left}
-            right={right}
-            background={background}>
-                <H4 labelsize={labelsize}>{label}</H4>
-                <P fontsize={fontsize} fontweight={fontweight}>{text}</P>
+                width={width} 
+                margin={margin}
+                position={position} 
+                top={top}
+                bottom={bottom}
+                left={left}
+                right={right}
+                background={background}>
+                    <H4 labelsize={labelsize}>{label}</H4>
+                    <P fontsize={fontsize} fontweight={fontweight}>{text}</P>
             </TextContainer>
-        </>
         )
     }
 
 Text.defaultProps = {
     fontsize: "15px",
-    fontweight: "300"
+    fontweight: "300",
+    height : "42px",
+    width: "100%"
 };
 
 const TextContainer = styled.div`
@@ -51,15 +89,62 @@ const TextContainer = styled.div`
     background-color: ${(props)=>props.background};
 `;
 
+const InlineTextContainer = styled.div`
+    display: inline-grid;
+    grid-template-columns: 30% 70%;
+    width: ${(props)=>props.width};
+    margin: ${(props)=>props.margin}; 
+    position: ${(props)=>props.position};
+    top: ${(props)=>props.top};
+    bottom: ${(props)=>props.bottom};
+    left: ${(props)=>props.left};
+    right: ${(props)=>props.right};
+    box-sizing: border-box;
+    background-color: ${(props)=>props.background};
+
+    @media (max-width: 200px){
+        display: inline-block;
+        width: ${(props)=>props.width};
+        margin: ${(props)=>props.margin}; 
+        position: ${(props)=>props.position};
+        top: ${(props)=>props.top};
+        bottom: ${(props)=>props.bottom};
+        left: ${(props)=>props.left};
+        right: ${(props)=>props.right};
+        box-sizing: border-box;
+        background-color: ${(props)=>props.background};
+    }
+`;
+
+const CenterTextContainer = styled.div`
+        display: inline-block;
+        width: ${(props)=>props.width};
+        margin: ${(props)=>props.margin}; 
+        position: ${(props)=>props.position};
+        top: ${(props)=>props.top};
+        bottom: ${(props)=>props.bottom};
+        left: ${(props)=>props.left};
+        right: ${(props)=>props.right};
+        box-sizing: border-box;
+        background-color: ${(props)=>props.background};
+        text-align: center;
+`;
+
 const H4 = styled.h4`
     font-size: ${(props)=>props.labelsize};
-    margin: 5px 0px 0px 10px;
+`;
+
+const InlineH4 = styled.h4`
+    font-size: ${(props)=>props.labelsize};
+    margin: 0px;
+    line-height: 42px;
 `;
 
 const P = styled.p`
     margin: 0;
     margin-top: 10px;
     padding-left: 10px;
+    text-align: center;
     font-size: ${(props)=>props.fontsize};
     font-weight: ${(props)=>props.fontweight};
 `;
