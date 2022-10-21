14cd023dc21e6c04ddf580c5805966d9b77b9c34
idx123 <jinhalee020@gmail.com>
2022-10-21 15:05:44 +0900

    [Feat] 파이차트 제거 및 식습관 문제, 해결식단 제안

--- src/pages/AnalyzeDiet.js
+++ src/pages/AnalyzeDiet.js
@@ -1,28 +1,35 @@
 import styled from "styled-components";
 import { useState,useEffect } from "react";
-import Grid from "../elements/Grid";
 import axios from "axios";
-import Pchart from "../components/Charts/Pchart";
+import Image from "../elements/Image";
 import WeeklyGraph from "../components/Charts/WeeklyGraph";
+import HorizontalStackGraph from "../components/Charts/HorizontalStackGraph";
 
 //식단분석 페이지
 function AnalyzeDiet(){
+    //오늘 날짜 출력을 위한 변수=======================================================================
     let today = new Date();   
-
     let year = today.getFullYear();
     let month = today.getMonth() + 1;
     let date = today.getDate();
     let fulldate = year + '.' + month + '.' + date;
 
+    //식이문제번호====================================================================================
+    const problem = ["칼로리 섭취 높음","칼로리 섭취 적음","탄수화물 섭취 높음","탄수화물 섭취 적음","단백질 섭취 높음","단백질 섭취 적음","지방 섭취 높음","지방 섭취 적음"];
+
+
+    //식단분석결과 가져오기(GET)=======================================================================
     const [ratio, setRatio] = useState('');
     const [total, setTotal] = useState('');
     const [weeklist, setWeeklist] = useState([]); //그래프에 사용(날짜별 총 섭취 칼로리)
+    const [problems, setProblems] = useState(''); //문제
+    const [suggestionList, setSuggestionList] = useState(''); //제안 식단
 
-    const getAnalysis=()=>{
-        axios.get("https://spring.chaebbiserver.shop/api/analysis",
+    const getAnalysis=async()=>{
+        await axios.get("https://spring.chaebbiserver.shop/api/analysis",
         { headers : { Authorization: `Bearer ${localStorage.getItem('token')}`}}
         ).then(function(response) {
-            // console.log(response.data.result);
+            console.log(response.data);
 
             setRatio({
                 carb: response.data.result.ratioCarb,
@@ -37,13 +44,13 @@ function AnalyzeDiet(){
             });
 
             setWeeklist(response.data.result.analysisDtos);
+            setProblems(response.data.result.problemsDtoList);
+            setSuggestionList(response.data.result.suggestionsDtoList);
             
         }).catch(function(error) {
             console.log(error);
         });
     }
-
-    
 
     useEffect(()=>{
         getAnalysis();
@@ -54,85 +61,105 @@ function AnalyzeDiet(){
         <>
         <H2>식단분석</H2>
         <Container>
-            <Grid col="2" row="1" colgap="10px">
-                    <ChartContainer>
+                <ChartContainer>
                     <h2>Today {fulldate}</h2>
-                    <P>지난 7일간 기록된 점수입니다.</P>
+                    <p>지난 7일간 기록된 점수입니다.</p>
                     {weeklist !== undefined||null||''?
-                        <div style={{width:"500px", height:"300px"}}><WeeklyGraph week={weeklist}/></div>
+                        <div style={{width:"550px", height:"300px", margin: "0px auto", position:"relative", left:"-18px"}}>
+                            <WeeklyGraph week={weeklist}/>
+                        </div>
                     :
-                        <div style={{margin: "0px auto", backgroundColor:"#e6e6e6"}}>표본 부족</div>
+                        <div style={{margin: "0px auto", backgroundColor:"#e6e6e6"}}>
+                            표본 부족
+                        </div>
                     }       
-                    </ChartContainer>
+                </ChartContainer>
 
                 <NutrientsInfomation>
                     <Title>주간영양섭취량</Title>
-
-                    <Grid col="2" row="1" margin="0">
-                        {ratio !== undefined||null||''?
-                            <Pchart nutrient={ratio}/>
-                    :
-                            <div style={{margin: "0px auto", backgroundColor:"#e6e6e6"}}>표본 부족</div>
-                        }
-                        <Grid col="2" row="1" margin="0">
-                            <h4 style={{color:"#0088fe"}}>탄수화물</h4>
-                            <p>{total.carb} g</p>
-                            <h4 style={{color:"#00c49f"}}>단백질</h4>
-                            <p>{total.protein} g</p>
-                            <h4 style={{color:"#ffbb28"}}>지방</h4>
-                            <p>{total.fat} g</p>
-                        </Grid>
-                    </Grid>
+                    {/* 아예 퍼센트를 계산해서 넘겨줘야 css를 인식할듯 */}
+                    <HorizontalStackGraph carb={total.carb} protein={total.protein} fat={total.fat} width="80%"/>
+                    <h3>지난 7일간의 식습관 평가</h3>
+                    <ul>
+                        {Array.from(problems).map((p,index) => (
+                            <li key={index}>{problem[p.problemId-1]}</li>
+                        ))}
+                    </ul>
                 </NutrientsInfomation>
-            </Grid>
-            
-            
 
-            
-
-            {/* <h4>평소보다 적게/많이 섭취하셨습니다.</h4> */}
+                <SuggestionContainer>
+                    <h2>식습관 개선을 위한 채식 제안</h2>
+                    <p>걱정마세요! 채삐가 사용자님을 위해 이런 음식들을 가져왔어요.</p>
+                    <GridContainer>
+                                {Array.from(suggestionList).map((s,index) => (
+                                    <div>
+                                        <div style={{height:"100px",backgroundColor:"yellow"}}>
+                                            <Image src={s.foodUrl} width="100%" height="auto"/>
+                                        </div>
+                                        <h3>{s.foodName}</h3>
+                                    </div>
+                                ))}
+                    </GridContainer>
+                </SuggestionContainer>
         </Container>
         </>
         )
     }
 
 const Container = styled.div`
-    width: 80%;
-    max-width: 1000px;
-    height: 460px;
-    min-width: 1000px;
+    width: 60%;
+    height: 1600px;
+    min-width: 600px;
     box-sizing: border-box;
-
-    margin: 0 auto;
     padding: 20px;
-
+    margin: 0 auto;
     position: relative;
     top: 70px;
-
     border: 1px solid #e6e6e6;
     border-radius: 15px;
 `;
 
 const ChartContainer = styled.div`
     width: 100%;
-    box-sizing: border-box;
-    padding-top: 20px;
-    padding-left: 10px;
-    /* border: 1px solid #e6e6e6; */
-    /* background-color: #e6e6e6; */
+    margin: 0 auto;
+
+    h2{ margin: 0;}
+    p{ margin: 10px 0; color: #868e96; }
 `;
 
 const NutrientsInfomation = styled.div`
-    min-width: 300px;
-    height: 300px;
-    box-sizing: border-box;
-    margin-top: 20px;
-    margin-bottom: 20px;
-    padding: 20px;
-    border: 1px solid #e6e6e6;
-    border-radius: 15px;
+    
+    ul li {
+
+    }
+`;
+
+const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, auto);
+    grid-template-rows: repeat(3, 200px);
+    column-gap: 10px;
+    row-gap: 10px;
     position: relative;
-    top: 30px;
+
+    div{
+        height: 200px;
+        border: 1px solid #e6e6e6;
+    }
+
+    div h3{
+        position: relative;
+        top: -50px;
+        z-index: 2;
+        color: #fff;
+    }
+
+
+`;
+
+const SuggestionContainer = styled.div`
+    h2{ margin: 20px 0 0 0; }
+    p{ margin: 10px 0; color: #868e96; }
 `;
 
 const H2 = styled.h2`
@@ -141,11 +168,6 @@ const H2 = styled.h2`
     position: relative;
     top: 70px;
     text-align: center;
-`;
-
-const P = styled.p`
-    margin: 0;
-    color: #868e96;
 `;
 
 const Title = styled.h2`
