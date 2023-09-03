e66be98b6638118a9162737f0b1a8e6504abf5a9
idx123 <jinhalee020@gmail.com>
2023-09-03 13:33:20 +0900

    [Feat] redux-toolkit 설치 및 설정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { GlobalStyle } from './styles/GlobalStyle'
+import { store } from './store/config';
 import App from './App';
 
-// ReactDOM.render(
-//     <App/>,
-//   document.getElementById('root')
-// )
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <Provider store={store}>
+            <GlobalStyle />
+            <App />
+        </Provider>
+      </BrowserRouter>
+    </React.StrictMode>
+  )
