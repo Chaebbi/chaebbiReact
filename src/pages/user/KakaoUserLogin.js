5836de910c729d119722313d5bc7e0488adc63df
idx123 <jinhalee020@gmail.com>
2024-01-07 21:08:34 +0900

    fix : 카카오 전용 유저 정보 입력페이지 유효성 검증 로직 수정

--- src/pages/user/KakaoUserLogin.js
+++ src/pages/user/KakaoUserLogin.js
@@ -1,14 +1,12 @@
-import styled from "styled-components";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import {API} from "../../utils/API.js";
-import {REST_API_KEY} from "../../utils/kakao_API_KEY.js";
 
 //카카오 로그인 페이지
 function KakaoUserLogin(){
     const location = useLocation();
     const navigate = useNavigate();
+    const client_id = process.env.REACT_APP_REST_API_KEY;
     const KAKAO_CODE = location.search.split('=')[1];
     const REDIRECT_URI = 'http://localhost:3000/kakao_login';
 
@@ -17,7 +15,7 @@ function KakaoUserLogin(){
         fetch(`https://kauth.kakao.com/oauth/token`,{
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' },
-            body: `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAO_CODE}`,
+            body: `grant_type=authorization_code&client_id=${client_id}&redirect_uri=${REDIRECT_URI}&code=${KAKAO_CODE}`,
         })
         .then(res => res.json())
         .then(data =>{
@@ -31,13 +29,13 @@ function KakaoUserLogin(){
 
     //카카오 액세스토큰 받아오기
     const sendKakaoToken=(at)=>{
-        axios.post(`${API}/login`,{
+        axios.post(`${process.env.REACT_APP_SERVER_URL}/api/login`,{
             accessToken: at
         },
         ).then(function(response) {
             console.log(response.data);
             localStorage.setItem('token', response.data.result.token);
-            if(response.data.result.signup == true){
+            if(response.data.result.signup === true){
                 navigate('/on_board');
             }else{
                 navigate('/');
